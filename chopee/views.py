ae74a2f3a8847c5f71ae5cb8c81344f522f9d0e7
ZOLARDEX14 <pomggfomy@gmail.com>
2026-07-09 16:58:13 +0700

    Update chopee templates to match assignment layout styling

--- chopee/views.py
+++ chopee/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def home(request):
-    return render(request, 'chopee/home.html')
+    products = Product.objects.all()
+    return render(request, 'chopee/home.html', {'products': products})
 
 def product_create(request):
     return render(request, 'chopee/product/create.html')
