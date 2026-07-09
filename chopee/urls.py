d3af4717937b53572f880cdec56273db8e2a046c
ZOLARDEX14 <pomggfomy@gmail.com>
2026-07-09 15:22:35 +0700

    Implement urls, views, and templates based on class lecture slides for chopee app

--- chopee/urls.py
+++ chopee/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from . import views
-
-app_name = 'chopee'
+from .views import *
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', home),
+    path('product/create', product_create),
+    path('product/retrieve', product_retrieve),
+    path('product/update', product_update),
+    path('product/delete', product_delete),
 ]
